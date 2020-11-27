01cc651954cfb47f01d4c0cfafc27c707da9c27d
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:04:25 -0300

    Add files via upload

---
+++ ListPOOex10/ListPOOex10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ListaPOOex10
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret1;
+
+            ret1 = new Retangulo();
+
+            Console.Write("Informe a base do retângulo: ");
+            ret1.setBase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe a altura do retângulo: ");
+            ret1.setAltura(int.Parse(Console.ReadLine()));
+
+            ret1.calcularArea();
+
+            Console.WriteLine("Um retângulo que tem base {0} e altura {1} tem área: {2} ",
+                ret1.getBase(), ret1.getAltura(), ret1.getArea());
+        }
+    }
+}
---
+++ ListPOOex10/ListPOOex10/Retangulo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex10
+{
+    class Retangulo
+    {
+        private int b;
+        private int h;
+        private int a;
+
+        public void setBase(int p)
+        {
+            b = p;
+        }
+
+        public void setAltura(int p)
+        {
+            h = p;
+        }
+
+        public int getBase()
+        {
+            return b;
+        }
+
+        public int getAltura()
+        {
+            return h;
+        }
+
+        public int getArea()
+        {
+            return a;
+        }
+
+        public void calcularArea()
+        {
+            a = b * h;
+
+            if (a > 100)
+    {
+                Console.WriteLine("Terreno grande");
+            }
+
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+
+        
+
+    }
+
+}
